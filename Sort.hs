a3d1cad70e52580df4bf13c7f6ac41457f0e608b
szymonpiotr <33660379+szymonpiotr@users.noreply.github.com>
2020-01-07 10:21:12 +0100

    Update Sort.hs

--- Sort.hs
+++ Sort.hs
@@ -3,7 +3,6 @@ module Sort where
 import Language
 import Rules
 
-
 sortList :: [For] -> ([For],[For],[For]) -> ([For],[For],[For])
 sortList [] (a,b,c) = (a,b,c)
 sortList (x:xs) (a,b,c)
@@ -11,6 +10,17 @@ sortList (x:xs) (a,b,c)
     | alpha x = sortList xs (a,x:b,c)
     | beta x  = sortList xs (a,b,x:c)
 
+sortFormula :: For -> ([For],[For],[For])
+sortFormula x = sortList [x] ([],[],[])
+
+sortCan :: For -> CanSeq
+sortCan x = Can $ sortFormula x
+
+dualize :: (a,a,a) -> (a,a,a)
+dualize (x,y,z) = (x,z,y)
+
+sortDual :: For -> DualSeq
+sortDual x = Dual $ dualize (sortFormula x)
 
 sort :: Cal -> [For] -> GeneralSeq
 sort cal xs = case cal of
