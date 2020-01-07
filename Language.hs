69b23d4186b1e95481d18767d220f3f85649060f
szymonpiotr <33660379+szymonpiotr@users.noreply.github.com>
2020-01-07 10:51:03 +0100

    Update Language.hs

--- Language.hs
+++ Language.hs
@@ -30,6 +30,28 @@ type HyperSeq = [GeneralSeq]
 
 data Cal = CanonicalRight | DualRight
 
+alpha :: For -> Bool
+alpha x = case x of
+        A _ _ -> True
+	N (D _ _) -> True
+	N ( I _ _) -> True
+        E _ _ -> True
+        _     -> False
+
+beta :: For -> Bool
+beta x = case x of
+        D _ _ -> True
+        I _ _ -> True
+	N (A _ _) -> True
+	N (E _ _) -> True
+        _     -> False
+
+var :: For -> Bool
+var x = case x of
+    V _ -> True
+    N (V _) -> True
+    _ -> False
+
 {-
 ruleAlpha :: CanSeq -> [CanSeq]
 
